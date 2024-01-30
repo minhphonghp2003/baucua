ed0dea25e4b9b9f079e09220944ff27ec260dc4b
minhphonghp2003 <minhphonghp2003@gmail.com>
2024-01-30 12:08:28 +0700

    new

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const { Client, GatewayIntentBits } = require("discord.js");
+const app = express();
+var cors = require("cors");
+
+const port = 3000;
+let KETQUA = "";
+require("dotenv").config(); //initializes dotenv
+const Discord = require("discord.js"); //imports discord.js
+
+const client = new Discord.Client({
+    intents: [
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+}); //creates new client
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", (msg) => {
+    let command = msg.content;
+    KETQUA = command.split("!gen ")[1]
+    console.log(KETQUA);
+});
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send(KETQUA);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+client.login(process.env.CLIENT_TOKEN); //signs the bot in with token
